cf4921423d425371483a3ee1a95d633e991be932
lemmoor <49235295+lemmoor@users.noreply.github.com>
2026-04-04 14:39:37 +0200

    bugfix: unnecessary scroll on mobile

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,7 @@ import ChatBox from "./components/ChatBox";
 export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center m-4">
-      <div className="w-full max-w-7xl h-[calc(100vh-2rem)]">
+      <div className="w-full max-w-7xl h-[calc(100dvh-2rem)]">
         <ChatBox />
       </div>
     </div>
